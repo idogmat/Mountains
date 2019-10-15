dfcdcf1ee7f6f60d12cf1793e1011716c900ff33
Jack <idogmat@gmail.com>
2019-10-15 17:33:34 +0300

    15.10

--- src/scripts/skills.js
+++ src/scripts/skills.js
@@ -1,1 +1,24 @@
-console.log('this is skills module');
+var parallax = (function () {
+    var bg = document.querySelector('.block__bg'),
+    user = document.querySelector('.block__ballum'),
+    sectionText = document.querySelector('.tablet');
+
+
+return {
+    move: function (block, windowScroll, stafeAmount) {
+        var strafe = windowScroll / -stafeAmount + '%';
+        var style = block.style;
+        style.top = strafe;
+    },
+    init: function (wScroll) {
+        this.move(bg, wSccroll, 2);
+    }
+
+    }
+}());
+
+window.onscroll = function(){
+    var wScroll = window.pageYOffset;
+    console.log(wScroll);
+    parallax.init(wScroll);
+}
