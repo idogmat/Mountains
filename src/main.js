95553fca4bbe61a630063e712dd69d18bfe4efca
Jack <idogmat@gmail.com>
2019-10-20 16:27:22 +0300

    Vue-slider2

--- src/main.js
+++ src/main.js
@@ -3,5 +3,7 @@ if (process.env.NODE_ENV === "development") {
   require("file-loader!./index.pug");
 }
 
-import "./scripts/skills";
 import "./scripts/parallax";
+import "./scripts/skills";
+import "./scripts/works";
+import "./scripts/slider-responses.js";
--- src/scripts/works.js
+++ src/scripts/works.js
@@ -1,21 +1,38 @@
 import Vue from "vue";
 
+
 const btns = {
     template: "#slider-btns"
 };
 const thambs = {
-    template: "#slider-thambs"
+    template: "#slider-thambs",
+    props:["works", "currentWork"],
+    computed: {
+        reverseWorks() {
+            return [...this.works].reverse();
+
+        }
+    }
 };
 const tags = {
-    template: "#slider-tags"
+		template: "#slider-tags",
+		props: ["tags"]
 };
 const display = {
     template: "#slider-display",
-    components:{ thambs, btns }
+    components: { thambs, btns },
+		props:["works", "currentWork","currentIndex"],
+		
 };
 const info = {
     template: "#slider-info",
-    components:{ tags }
+    components: { tags },
+		props:["currentWork"],
+		computed: {
+			tagsArray() {
+				return this.currentWork.skills.split(', ');
+			}
+		}
 };
 new Vue({
     template: "#slider-container",
@@ -23,4 +40,50 @@ new Vue({
     components: {
       display,
       info
-    };
+    },
+    data:() => ({
+        works:[],
+        currentIndex: 0
+
+		}),
+		computed: {
+			currentWork() {
+				return this.works[this.currentIndex];
+			}
+		},
+		watch: {
+			currentIndex(value) {
+				this.makeInfiniteLoopForCurIndex(value);
+			}
+		},
+    methods: {
+			makeInfiniteLoopForCurIndex(value) {
+				const worksAmount = this.works.length - 1;
+				if (value > worksAmount) this.currentIndex = 0;
+				if (value < 0) this.currentIndex = worksAmount;
+			},
+        makeArrWithRequiredImages(data) {
+            return data.map(item => {
+                const requiredPic = require(`../images/content/${item.photo}`);
+                item.photo = requiredPic;
+                return item
+            })
+				},
+        handleSlide(direction) {
+					switch (direction) {
+						case "next":
+							this.currentIndex++;
+							break;
+						case "prev":
+							this.currentIndex--;
+							break;
+					}
+        }
+    },
+    created(){
+        const data = require("../data/works.json");
+        this.works = this.makeArrWithRequiredImages(data);
+
+        this.currentWork = this.works[this.currentIndex];
+    }
+});
